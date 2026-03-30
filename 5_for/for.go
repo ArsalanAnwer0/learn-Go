7e1dbd282a85fb1f4c3c2e4e8522fda0259efdd4
Muhammad Arsalan <arsalan.anwer9050@gmail.com>
2026-03-29 19:44:58 -0500

    for loop

---
+++ 5_for/for.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// for -> only contruct in loop for looping
+
+func main() {
+
+	// while loop
+
+	// i := 1
+	// for i <= 3 {
+	// 	fmt.Println(i)
+	// 	i = i + 1
+	// }
+
+	// infinite loop
+	// for {
+	// 	println(1)
+	// }
+
+	// classic for loop
+
+	// for i := 0; i <= 3; i++ {
+	// 	if i == 2 {
+	// 		continue
+	// 	}
+	// 	println(i)
+	// }
+
+	for i := range 11 {
+		fmt.Println(i)
+	}
+
+}
