a103154487e0881826235297e0b17e0bce118a2c
Muhammad Arsalan <arsalan.anwer9050@gmail.com>
2026-04-04 15:18:56 -0500

    array

---
+++ 8_arrays/arrays.go
@@ -0,0 +1,33 @@
+package main
+
+// numbered sequence of values of the same type and fixed length
+
+func main() {
+	// var nums [4]int
+
+	// fmt.Println(len(nums))
+
+	// nums[0] = 1
+
+	// fmt.Println(nums[0])
+
+	// var vals [4]bool
+	// vals[2] = true
+	// fmt.Println(vals)
+
+	// var names [3]string
+	// names[0] = "Alice"
+	// fmt.Println(names)
+
+	// // to declare it in single line
+	// nums := [3]int{1, 2, 3}
+	// fmt.Println(nums)
+
+	// two-dimensional array
+	// nums := [2][2]int{{3, 3}, {5, 6}}
+	// fmt.Println(nums)
+	// if fixed size, that is predictable
+	// Memory optimization, faster access, better cache performance
+	// constant time access to elements
+
+}
