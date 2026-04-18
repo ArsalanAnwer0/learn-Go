66f87e8ad14ce4435327632b2e4402897a4c3706
Muhammad Arsalan <arsalan.anwer9050@gmail.com>
2026-04-17 19:12:23 -0500

    complete run

---
+++ basics/main.go
@@ -0,0 +1,17 @@
+// compiled? -> interpreted?
+// go lang is compiled.
+// Go tool can run file directly, without VM
+// Executables are different for OS.
+// what and where can I use go lang? system apps to web app - Cloud
+// object oriented -> Yes and No -> Structs -> no operating loading.
+// prev -> setup path, now -> run code from anywhere..
+// What is Go Path..
+//
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Arsalan")
+}
