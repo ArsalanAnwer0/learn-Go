c47c5faa321f9dffe32f2b97d08623cc451c6a6e
Muhammad Arsalan <arsalan.anwer9050@gmail.com>
2026-04-21 14:54:46 -0500

    array

--- basics/main.go
+++ basics/main.go
@@ -111,4 +111,20 @@ func main() {
 	fmt.Println("Value of pointer is: ", *ptr2)
 	fmt.Println("Value of num is: ", num)
 
+	// Array
+	fmt.Println("Welcome to array in golangs")
+
+	var fruitlist [4]string
+
+	fruitlist[0] = "Apple"
+	fruitlist[1] = "Tomato"
+	fruitlist[3] = "Peach"
+
+	fmt.Println("Fruit List is: ", fruitlist)
+	fmt.Println("Fruit List is: ", len(fruitlist))
+
+	var vegList = [3]string{"Potato", "beans", "mushroom"}
+	fmt.Println("Vegy list is ", vegList)
+	fmt.Println("Vegy list is ", len(vegList))
+
 }
