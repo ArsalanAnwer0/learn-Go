6506d8f2df0ce3feaa3cd2eb57e3059a312d673e
Muhammad Arsalan <arsalan.anwer9050@gmail.com>
2026-04-20 19:56:34 -0500

    consts and variables

--- basics/main.go
+++ basics/main.go
@@ -6,12 +6,46 @@
 // object oriented -> Yes and No -> Structs -> no operating loading.
 // prev -> setup path, now -> run code from anywhere..
 // What is Go Path..
-//
+// Lexer? ->
+// types -> private or public,
+// data types -> string, bool, integer, floating, complex, array, slices, maps, structs and
+// pointers
+// Function and Channels
 
 package main
 
 import "fmt"
 
+const LoginToken string = "blah" // capital L -> public variable
+
 func main() {
-	fmt.Println("Arsalan")
+	// fmt.Println("Arsalan")
+
+	// variables:
+	var username string = "Arsalan"
+	var isLoggedIn bool = true
+	fmt.Println(username)
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n", username)
+
+	var smallFloat float64 = 255.455444353
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is of type: %T \n", smallFloat)
+
+	// default values and some aliases -> always zero, initialized and not assigned
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable is of type: %T \n", anotherVariable)
+
+	// implicit type -> if not mentioned, i will decide for you (lexer)
+	var website = "learncodeonline"
+	fmt.Println(website)
+
+	// no var style, not allowed outside
+	numberOfUser := 300000
+	fmt.Println(numberOfUser)
+
+	fmt.Println(LoginToken)
+	fmt.Printf("Variable is of type: %T \n", LoginToken)
+
 }
