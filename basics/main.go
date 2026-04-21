e0a1179e31f1382021875ee53e146a7fcd3522b6
Muhammad Arsalan <arsalan.anwer9050@gmail.com>
2026-04-21 13:15:09 -0500

    conversions

--- basics/main.go
+++ basics/main.go
@@ -18,6 +18,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 const LoginToken string = "blah" // capital L -> public variable
@@ -64,5 +66,21 @@ func main() {
 	input, _ := reader.ReadString('\n')
 	fmt.Println("Thanks for rating", input)
 	fmt.Printf("Type of rating is %T", input)
+
+	// conversions
+	fmt.Println("Welcome to our pizza app")
+	fmt.Println("Please rate our pizza between 1 and 5")
+
+	reader_two := bufio.NewReader(os.Stdin)
+	input_two, _ := reader_two.ReadString('\n')
+
+	fmt.Println("Thanks for rating, ", input_two)
+	numRating, errr := strconv.ParseFloat(strings.TrimSpace(input_two), 64)
+
+	if errr != nil {
+		fmt.Println(errr)
+	} else {
+		fmt.Println("Added 1 to your rating: ", numRating+1)
+	}
 
 }
