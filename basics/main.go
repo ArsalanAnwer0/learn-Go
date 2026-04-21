2ac0ff8050599d85fb6c9d0e33b82f6fba3ed4dd
Muhammad Arsalan <arsalan.anwer9050@gmail.com>
2026-04-20 20:08:39 -0500

    comma ok syntax

--- basics/main.go
+++ basics/main.go
@@ -14,7 +14,11 @@
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 const LoginToken string = "blah" // capital L -> public variable
 
@@ -47,5 +51,18 @@ func main() {
 
 	fmt.Println(LoginToken)
 	fmt.Printf("Variable is of type: %T \n", LoginToken)
+
+	// user input using package bufio
+	welcome := "Welcome to user input"
+	fmt.Println(welcome)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating for our pizza:")
+
+	// comma ok | error ok syntax
+
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for rating", input)
+	fmt.Printf("Type of rating is %T", input)
 
 }
